68fd42cc3c3133e3697c08ce76b5ea6151d1578b
iman <irwebuniq@gmail.com>
2025-11-27 09:58:49 +0330

    Update blockchain chains to use BSC and BSC Testnet in configuration.

--- src/wagmi.ts
+++ src/wagmi.ts
@@ -1,13 +1,12 @@
 import { createConfig, createStorage, http } from '@wagmi/vue'
-import { mainnet, optimism, sepolia } from '@wagmi/vue/chains'
+import { bsc, bscTestnet } from '@wagmi/vue/chains'
 
 export const config = createConfig({
-  chains: [mainnet, sepolia, optimism],
+  chains: [bsc, bscTestnet],
   storage: createStorage({ storage: localStorage, key: 'vite-vue' }),
   transports: {
-    [mainnet.id]: http(),
-    [sepolia.id]: http(),
-    [optimism.id]: http(),
+    [bsc.id]: http(),
+    [bscTestnet.id]: http(),
   },
 })
 
