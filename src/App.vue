cf9bbe8e0707a7410afdb5ae33a1be71096205b6
bitameshgi <meshgibita@gmail.com>
2025-11-27 18:11:48 +0330

    feat: add welcome text section

--- src/App.vue
+++ src/App.vue
@@ -3,7 +3,25 @@
     <div class="w-[380px] min-h-[700px] overflow-hidden">
       <HeroCard />
       <DepositCard />
-      <RewardCard/>
+      <RewardCard />
+      <div class="text-white p-6 my-6">
+        <p class="text-base font-semibold">Welcome to USDBIT</p>
+        <p class="text-2xl font-extrabold">
+          Your Smart Financial
+        </p>
+        <p class="text-2xl font-extrabold text-primary">
+          Experience
+        </p>
+        <p class="text-justify mt-4 text-base leading-loose text-gray-400">
+          Welcome to usdbit – your ultimate gateway to powerful Polygon-based investments! At PolyCash, we offer a
+          high-yield opportunity with 3.5% daily ROI for 100 days — starting with just 10 POL. Our smart system ensures
+          maximum growth through transparent, reliable, and automated returns. Plus, with our 4-level referral program,
+          you
+          earn even more by inviting others: 5% - 3% - 2% - 1% — directly to your wallet. Join PolyCash today and
+          experience
+          next-level staking — where smart investing meets real rewards.
+        </p>
+      </div>
     </div>
   </div>
 </template>
