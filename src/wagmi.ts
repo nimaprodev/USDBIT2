a57c1ceb4c67d2bf4c1a15eceeb3f32950e80f41
iman <irwebuniq@gmail.com>
2025-11-27 13:11:19 +0330

    Add balance tracking and update connection configuration in Vue components.

--- src/wagmi.ts
+++ src/wagmi.ts
@@ -2,10 +2,11 @@ import { createConfig, createStorage, http } from '@wagmi/vue'
 import { bsc, bscTestnet } from '@wagmi/vue/chains'
 
 export const config = createConfig({
-  chains: [bsc, bscTestnet],
+  // chains: [bsc, bscTestnet],
+  chains: [bscTestnet],
   storage: createStorage({ storage: localStorage, key: 'vite-vue' }),
   transports: {
-    [bsc.id]: http(),
+    // [bsc.id]: http(),
     [bscTestnet.id]: http(),
   },
 })
