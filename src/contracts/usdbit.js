b050c92f74c35023ad0aa4e1fae849178a30add9
iman <irwebuniq@gmail.com>
2025-11-28 08:39:16 +0330

    Update...

--- src/contracts/usdbit.js
+++ src/contracts/usdbit.js
@@ -1,6 +1,3 @@
-
-export const usdbitContractAddress = '0x5FbDB2315678afecb367f032d93F642f64180aa3';
-
-
 export const usdtTokenAddress = '0x472a11E85c992fECC4C36B3B417935821F68F753';
 
+export const usdbitContractAddress = '0xd3ACCdC670a0290021932C7451C4bad79e6A5F19';
