55820cc8047de0e6f5be38b15b90088bdb2793ed
nimaprodev <nimapro86@gmail.com>
2025-11-30 09:20:06 +0330

    remove RewardCard and integrate its functionality into App.vue (main)

--- src/App.vue
+++ src/App.vue
@@ -94,7 +94,40 @@
       </div>
       <!--      <DepositCard />-->
 
-      <RewardCard/>
+      <!--      RewardCard -->
+      <div class="relative text-center overflow-hidden">
+        <img :src="rewardImg" alt="Reward Image" class="object-cover w-full h-auto"/>
+
+        <div class="absolute inset-0 flex flex-col justify-end items-center text-center px-4 pb-6">
+
+          <div class="bg-gray-100 p-6 rounded-md w-full">
+
+            <div class="flex items-start gap-4 p-2 rounded-md">
+
+              <div class="w-14 h-14 rounded-md bg-primary flex items-center justify-center  mt-1">
+                <img :src="walletMoney" alt="walletMoney" class="object-contain"/>
+              </div>
+
+              <div class="flex flex-col text-left">
+                <p class="text-primary font-semibold text-sm">Your Reward</p>
+
+                <span class="text-xl font-semibold text-white leading-none mt-3">
+                            {{ formatDisplayNumber(your_reward, 6) }} USDT
+                        </span>
+                <button v-loading="isWithdrawRewardLoading" @click="withdrawReward"
+                        class="text-white text-sm mt-5 inline-flex items-center justify-center bg-secondary p-2 px-5 rounded">
+                            <span class="inline-flex items-center">
+                                Withdraw Now
+                                <img :src="arrowRight" alt="arrow right" class="ml-4 align-middle"/>
+                            </span>
+                </button>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+      <!--      <RewardCard/>-->
+
       <div class="text-white p-6 my-6">
         <p class="text-base font-semibold">Welcome to USDBIT</p>
         <p class="text-2xl font-extrabold">
@@ -113,7 +146,32 @@
           next-level staking — where smart investing meets real rewards.
         </p>
       </div>
-      <ReferralCard/>
+<!--      ReferralCard -->
+      <div class="bg-gray-100 p-6 w-full">
+        <img :src="referralImg" alt="referral Image" class="object-cover mb-4" />
+
+        <div class="flex items-center text-2xl font-extrabold gap-2 mb-4">
+            <span class="text-white">Your</span>
+            <span class="text-primary">referral link</span>
+        </div>
+
+        <p class="text-justify text-base leading-loose text-gray-400 mb-6">
+            Share your unique link and earn rewards every time someone joins through you. Invite friends, build your
+            network,
+            and grow together—success is better when shared.
+        </p>
+
+        <div class="relative w-full mb-10">
+            <input readonly :value="referralLink"
+                class="w-full pl-4 py-3 rounded-xl bg-gray-50 text-white focus:outline-none focus:ring-2 focus:ring-primary" />
+            <button
+                class="absolute right-2 top-1/2 -translate-y-1/2 w-10 h-10 rounded-xl bg-primary flex items-center justify-center"
+                @click="copyToClipboard">
+                <img :src="clipboard" alt="clipboard" class="w-5 h-5" />
+            </button>
+        </div>
+    </div>
+<!--      <ReferralCard/>-->
       <TotalCommissionCard/>
       <div class="text-white p-6 my-6">
         <p class="text-base font-semibold">Level System</p>
@@ -143,6 +201,14 @@ import logo from './assets/images/logo.svg'
 import {useAccount, useConnect, useDisconnect, useConnectors} from '@wagmi/vue'
 import {useDepositCard} from './composables/useDepositCard.js';
 import trendUp from './assets/images/trend-up.svg';
+import TotalCommissionCard from './components/TotalCommissionCard.vue'
+import Footer from './components/Footer.vue'
+import rewardImg from "./assets/images/reward.png";
+import walletMoney from "./assets/images/wallet-money.svg";
+import arrowRight from "./assets/images/arrow-right.svg";
+import referralImg from './assets/images/referral.png'
+import clipboard from './assets/images/clipboard.svg'
+import { useUser } from './composables/useUser.js';
 
 const {address, isConnected} = useAccount()
 const {connect} = useConnect()
@@ -156,12 +222,29 @@ const shortAddress = computed(() => {
   return ''
 })
 
+const {your_reward, withdrawReward, isWithdrawRewardLoading} = useUser();
+const referral_code = computed(() => {
+  // The getUserInfo function returns an array. Assuming referralCode is the 4th element (index 3).
+  // Please adjust the index based on your contract's struct.
+  // if (userInfo && userInfo.value && userInfo.value[3]) {
+  //   return userInfo.value[3];
+  // }
+  return null;
+});
 
-import RewardCard from './components/RewardCard.vue'
-import ReferralCard from './components/ReferralCard.vue'
-import TotalCommissionCard from './components/TotalCommissionCard.vue'
-import Footer from './components/Footer.vue'
+const referralLink = computed(() => {
+  if (referral_code.value) {
+    return `${window.location.protocol}//${window.location.host}/?ref=${referral_code.value}`;
+  }
+  return "";
+});
 
+const copyToClipboard = () => {
+    if (referralLink.value) {
+        navigator.clipboard.writeText(referralLink.value)
+    alert("Copied!")
+}
+}
 const {
   total_deposit,
   total_withdraw,
